4590fa7c90d1c7e0096dd2bd78037add8ae20caa
Mihir Thalanki <mihir.thalanki@gmail.com>
2025-11-05 10:24:17 -0500

    Add frame buffers

--- src/models/KeyMomentsFinder.py
+++ src/models/KeyMomentsFinder.py
@@ -18,9 +18,13 @@ class KeyMomentsFinder:
     def find_key_moments(self, config):
         match_id = config['match_id']
         sequence_func = config['sequence_func']
+        start_buffer = config.get('start_buffer', 0)
+        end_buffer = config.get('end_buffer', 0)
         column_aggregations = config.get('column_aggregations', {})
         events_data = self.import_data(match_id)
         events_with_sequence_data = self.create_sequence_column(match_id, sequence_func, events_data)
         events_with_sequence_data = events_with_sequence_data[list(column_aggregations.keys()) + ['Sequence_ID']]
         grouped_data = events_with_sequence_data.groupby('Sequence_ID').agg(column_aggregations).reset_index()
+        grouped_data['frame_start'] = grouped_data['frame_start'] - start_buffer
+        grouped_data['frame_end'] = grouped_data['frame_end'] + end_buffer
         return grouped_data
