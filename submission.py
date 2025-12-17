f7cd261e61372a6fd991709efa0f6358cef861a0
Mihir Thalanki <mihir.thalanki@gmail.com>
2025-12-17 18:44:31 -0500

    Enable hot reload

--- submission.py
+++ submission.py
@@ -1,10 +1,12 @@
 from src.models.DashInteraction import DashInteraction
 
 def create_dash_app():
-    da = DashInteraction(episode_file='src/episodes/custom_episodes/episode_10_100.json')
+    print("Creating dash app")
+    #da = DashInteraction(episode_file='src/episodes/custom_episodes/episode_10_100.json')
+    da = DashInteraction(episode_file='src/episodes/1886347_all_shots/episode_2.json')
     app = da.create_app()
     return app
 
 if __name__ == "__main__":
     app = create_dash_app()
-    app.run(debug=True, port=8051)
+    app.run(debug=True, port=8051, dev_tools_hot_reload=True)
