75c8e22675a0ebfb95dc613e39886878a08f9384
Mihir Thalanki <mihir.thalanki@gmail.com>
2025-12-20 10:16:17 -0500

    Restructure

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,7 @@
 from .DataLoader import DataLoader
-from .GameRenderer import GameRenderer
 from .KeyMomentsFinder import KeyMomentsFinder
 from .Pitch import Pitch
-from .PlotlyGameRenderer import PlotlyGameRenderer
 from .DashPlotlyGameRenderer import DashPlotlyGameRenderer
 from .DashInteraction import DashInteraction
 
-__all__ = ['DataLoader', 'GameRenderer', 'KeyMomentsFinder', 'Pitch', 'PlotlyGameRenderer', 'DashPlotlyGameRenderer', 'DashInteraction']
+__all__ = ['DataLoader', 'KeyMomentsFinder', 'Pitch', 'DashPlotlyGameRenderer', 'DashInteraction']
